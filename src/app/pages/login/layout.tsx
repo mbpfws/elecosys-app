81c51a1b728706aa07293390d21cdc1c45873126
mbpfws <60363657+mbpfws@users.noreply.github.com>
2025-05-02 13:27:57 +0700

    Simplify login page layout

--- src/app/pages/login/layout.tsx
+++ src/app/pages/login/layout.tsx
@@ -1,11 +1,9 @@
 'use client'
 
-import BlankLayout from '@layouts/BlankLayout'
-
 export default function LoginLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return <BlankLayout>{children}</BlankLayout>
+  return <>{children}</>
 }
